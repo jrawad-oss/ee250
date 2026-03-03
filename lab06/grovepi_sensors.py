d58dea9100d8593576a83414e14400fa29586fa0
Jessica Awad <jrawad@usc.edu>
2026-03-03 00:31:34 +0000

    fixed grovepi file

--- lab06/grovepi_sensors.py
+++ lab06/grovepi_sensors.py
@@ -1,33 +1,44 @@
 import sys
-sys.path.append('~/Dexter/GrovePi/Software/Python')
+import os
+sys.path.append(os.path.expanduser('~/Dexter/GrovePi/Software/Python'))
 import time
 import grovepi
 from grove_rgb_lcd import *
 
-# Grove Ultrasonic Ranger connectd to digital port 3 
-ultrasonic_ranger = 3
+# Grove Ultrasonic Ranger connectd to digital port 2
+ultrasonic_ranger = 2
 # potentiometer connected to analog port A0 as input
 potentiometer = 0
 grovepi.pinMode(potentiometer,"INPUT")
 
-# clear lcd screen  before starting main loop
+# clear lcd screen before starting main loop
 setText("")
 
 while True:
   try:
     # TODO:read distance value from Ultrasonic Ranger and print distance on LCD
     distance = grovepi.ultrasonicRead(ultrasonic_ranger)
+    if distance is None or distance <= 0:
+      distance = 0
 
     # TODO: read threshold from potentiometer
     threshold = grovepi.analogRead(potentiometer)
+    if threshold is None:
+      threshold = 0
 
-    # clear present for every iteration
-    present = ""
+    threshold_in_cm = float(threshold) * 517.0 / 1023.0
+
     # TODO: format LCD text according to threshhold
-    if distance < threshold:
+    if float(distance) < threshold_in_cm:
       present = "OBJ PRES"
     else:
       present = ""
-    setText_norefresh(str(threshold) + " cm " + present + "\n" + str(distance) + " cm") # norefresh ensures the LCD not to be blinking as it will not refresh each time new text is displayed
+
+    top = (str(int(threshold)) + " " + present).ljust(16)
+    bottom = str(int(distance)) + " cm"
+
+    setText_norefresh(top + "\n" + bottom)
+    time.sleep(0.18)
+
   except IOError:
     print("Error")
